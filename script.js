1124a866ffaf09b6c144737bffbc73dc598f736a
budapest85 <alvarogarciaygomez@gmail.com>
2024-06-09 13:08:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const questions = [
 
 let currentQuestionIndex = 0;
 const chatBox = document.getElementById('chat-box');
-const scriptUrl = 'https://script.google.com/macros/s/AKfycbzQLwp7Frs9IJwGXQ6oDh1crAxmDKKh0qSPPlic1qmEPO-wJidp4OZt_1I_gZYLm1u1/exec'; // Reemplaza con la URL del script de Google Apps
+const scriptUrl = 'https://script.google.com/macros/s/AKfycbzxW7VX8UVCX3d_rJlfuA0cAUceep95sfINdsUR-EjPiFKpRJXLjlIDsBYr9FOWIdXg/exec'; // Reemplaza con la URL del script de Google Apps
 
 function addMessageToChat(message, sender, answers) {
   const messageElement = document.createElement('div');

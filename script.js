172ccc4b00d06f64b39baff658cbc7fdd1adfae0
budapest85 <alvarogarciaygomez@gmail.com>
2024-06-09 12:20:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function validateEmail(email) {
 }
 
 async function saveAnswerToGoogleSheets(question, answer) {
-  const sheetId = '10kyTZwBYH4r4oxLY0YCdpiB1WFVWCPRDEvpP8QIGqjk'; // Reemplaza con tu ID de la hoja de Google
+  const sheetId = '1SUFNHZBTs6aNkOUS-MjMgMEP2h0XCVzhleWKvBv9kaQ'; // Reemplaza con tu ID de la hoja de Google
   const apiKey = 'AIzaSyB0LcBVw0dR2DXWBIoXoH04OhKalAhmq60'; // Reemplaza con tu clave de API
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/A1:append?valueInputOption=USER_ENTERED&key=${apiKey}`;
 
